36fa8dc8d9e96cbe3983a672e1e2c2ad1bd7e39e
Ritish Timalsina <ritishtimalsina78@gmail.com>
2023-07-05 19:30:39 +0545

    added header and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        <p>ByteBuy &copy; {currentYear}</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer;
