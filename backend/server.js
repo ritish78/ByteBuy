10a60f62ef496069077c9cf399e76093e09fdaef
Ritish Timalsina <ritishtimalsina78@gmail.com>
2023-07-31 18:44:05 +0545

    finally finally finally or hopefully

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use(cors(corsOptions));
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use('/uploads', express.static('/var/data/uploads'));
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
   
     app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
     );
 } else {
     app.get('/', (req, res) => {
